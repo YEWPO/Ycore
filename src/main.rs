f1fab51171dc882bfe373559658f8428afe9f6e0
22050908-Meng Pei <2398696309@qq.com>
2024-01-23 12:26:59 +0800

    println

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@
 use core::arch::global_asm;
 
 mod lang_items;
+mod console;
+mod sbi;
 
 global_asm!(include_str!("entry.S"));
 
 #[no_mangle]
 fn kernel_main() {
-    loop {}
+    println!("Hello, world!");
+    panic!();
 }
