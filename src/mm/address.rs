9fc91659c932252fbf071d08a86a4c34d2fe24e4
22050908-Meng Pei <2398696309@qq.com>
2024-01-24 17:21:58 +0800

    virt add & phys addr ops

--- src/mm/address.rs
+++ src/mm/address.rs
@@ -1,6 +1,4 @@
-use core::fmt::Debug;
-
-use crate::config::{VA_WIDTH, PA_WIDTH, VPN_WIDTH, PPN_WIDTH};
+use crate::config::{VA_WIDTH, PA_WIDTH, VPN_WIDTH, PPN_WIDTH, PAGE_SIZE_BITS};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct VirtAddr(pub usize);
@@ -63,5 +61,65 @@ impl From<VirtPageNum> for usize {
 impl From<PhysPageNum> for usize {
     fn from(value: PhysPageNum) -> Self {
         value.0
+    }
+}
+
+impl VirtAddr {
+    pub fn floor(&self) -> VirtPageNum {
+        VirtPageNum(self.0 >> PAGE_SIZE_BITS)
+    }
+
+    pub fn ceil(&self) -> VirtPageNum {
+        VirtPageNum((self.0.checked_sub(1).unwrap_or(0) + ((1 << PAGE_SIZE_BITS) - 1)) >> PAGE_SIZE_BITS)
+    }
+
+    pub fn page_offset(&self) -> usize {
+        self.0 & ((1 << PAGE_SIZE_BITS) - 1)
+    }
+
+    pub fn aligned(&self) -> bool {
+        self.page_offset() == 0
+    }
+}
+
+impl From<VirtPageNum> for VirtAddr {
+    fn from(value: VirtPageNum) -> Self {
+        VirtAddr::from(value.0 << PAGE_SIZE_BITS)
+    }
+}
+
+impl From<VirtAddr> for VirtPageNum {
+    fn from(value: VirtAddr) -> Self {
+        value.floor()
+    }
+}
+
+impl PhysAddr {
+    pub fn floor(&self) -> PhysPageNum {
+        PhysPageNum(self.0 >> PAGE_SIZE_BITS)
+    }
+
+    pub fn ceil(&self) -> PhysPageNum {
+        PhysPageNum((self.0.checked_sub(1).unwrap_or(0) + ((1 << PAGE_SIZE_BITS) - 1)) >> PAGE_SIZE_BITS)
+    }
+
+    pub fn page_offset(&self) -> usize {
+        self.0 & ((1 << PAGE_SIZE_BITS) - 1)
+    }
+
+    pub fn aligned(&self) -> bool {
+        self.page_offset() == 0
+    }
+}
+
+impl From<PhysPageNum> for PhysAddr {
+    fn from(value: PhysPageNum) -> Self {
+        PhysAddr::from(value.0 << PAGE_SIZE_BITS)
+    }
+}
+
+impl From<PhysAddr> for PhysPageNum {
+    fn from(value: PhysAddr) -> Self {
+        value.floor()
     }
 }
