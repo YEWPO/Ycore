c5c3a2cbce0e0a624eace1cd2176b88a77a91311
22050908-Meng Pei <2398696309@qq.com>
2024-03-12 17:28:25 +0800

    feat: add process syscall framework

--- src/syscall/mod.rs
+++ src/syscall/mod.rs
@@ -1,12 +1,18 @@
 mod fs;
+mod process;
 
 use fs::*;
+use process::*;
 
 const SYS_OPENAT: usize             = 56;
 const SYS_CLOSE: usize              = 57;
 const SYS_LSEEK: usize              = 62;
 const SYS_READ: usize               = 63;
 const SYS_WRITE: usize              = 64;
+
+const SYS_EXIT: usize               = 93;
+const SYS_SCHED_YIELD: usize        = 124;
+const SYS_GETTIMEOFDAY: usize       = 169;
 
 pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
     match syscall_id {
@@ -15,6 +21,11 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
         SYS_LSEEK               => sys_lseek(),
         SYS_READ                => sys_read(),
         SYS_WRITE               => sys_write(),
-        _ => panic!("Unsupport syscall_id: {}", syscall_id),
+
+        SYS_EXIT                => sys_exit(),
+        SYS_SCHED_YIELD         => sys_sched_yield(),
+        SYS_GETTIMEOFDAY        => sys_gettimeofday(),
+
+        _                       => panic!("Unsupport syscall_id: {}", syscall_id),
     }
 }
---
+++ src/syscall/process.rs
@@ -0,0 +1,11 @@
+pub fn sys_exit() -> isize {
+    0
+}
+
+pub fn sys_sched_yield() -> isize {
+    0
+}
+
+pub fn sys_gettimeofday() -> isize {
+    0
+}
